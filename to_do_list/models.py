f88e00f2584a0bc2e0f844104db4db85457b9f3c
Igor Bedon de Sousa <igorbedon@gmail.com>
2024-11-04 01:40:38 -0300

    Creating base models of Easy Task

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Environment(models.Model):
+    title = models.CharField(max_length=100)  
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)  
+    description = models.TextField()
+    status = models.CharField(max_length=25)
+    priority = models.IntegerField()
+    deadline = models.DateField()
+    environment = models.ForeignKey(Environment, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+    
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)  
+    description = models.TextField()
+    color = models.CharField(max_length=25)
+    environment = models.ForeignKey(Environment, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+    
+
+class Attachment (models.Model):
+    title = models.CharField(max_length=100)  
+    description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to='attachments/')
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
